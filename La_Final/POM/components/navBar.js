b0869abcd22e52958a04d6a418c76a1483f16d2d
NinaKiselyeva <157054656+NinaKiselyeva@users.noreply.github.com>
2024-05-19 22:24:26 +0200

    Update navBar.js

--- La_Final/POM/components/navBar.js
+++ La_Final/POM/components/navBar.js
@@ -9,13 +9,18 @@ class NavBar extends Base {
     return $('a.navbar-brand');
   }
 
+  get addToCartNotification() {
+    return $('div#toast-container');
+  }
+
   get buttonCart() {
-    return $('a[data-test="nav-cart"]');
+    return $('a[data-test="nav-cart"] span');
   }
 
   async getCartCounter() {
-    await this.buttonCart.waitForExist();
-    return this.buttonCart.getValue();
+    await this.addToCartNotification.waitForDisplayed();
+    await this.buttonCart.waitForDisplayed();
+    return parseInt(await this.buttonCart.getText());
   }
 
   async subCategories(categoryName) {
@@ -25,44 +30,12 @@ class NavBar extends Base {
   async clickMainMenuBtn(buttonName) {
     const button = await this.mainMenuBtn(buttonName);
     await this.baseClick(button);
-    // await button.waitForExist({ timeout: 10000 });
-    // await button.click();
   }
 
   async clickCategoryOption(categoryName) {
-    //this.clickMainMenuBtn('Categories');
     const category = await this.subCategories(categoryName);
     await this.baseClick(category);
-    // await category.waitForExist();
-    // await category.click();
   }
-
-  //     get searchButton() {
-  //     return $("//button[@class='DocSearch DocSearch-Button']");
-  //   }
-
-  //   get modeButton() {
-  //     return $("//button[@class='clean-btn toggleButton_gllP']");
-  //   }
-
-  //   get modeButton() {
-  //     return $("//button[@class='clean-btn toggleButton_gllP']");
-  //   }
-
-  //   async goToModeButton() {
-  //     await this.modeButton.waitForDisplayed();
-  //     await this.modeButton.moveTo();
-  //   }
-
-  //   async clickModeButton() {
-  //     await this.modeButton.waitForDisplayed();
-  //     await this.modeButton.click();
-  //   }
-
-  //   async goToSearchModalFromKeyboard() {
-  //     await this.searchButton.waitForDisplayed({ timeout: 2000 });
-  //     await browser.keys([Key.Ctrl, 'k']);
-  //   }
 }
 
 module.exports = new NavBar();
